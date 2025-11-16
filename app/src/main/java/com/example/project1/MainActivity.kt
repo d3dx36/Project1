b11e494b27865226fdee13a517bef7acec7b4246
yuri <mortus259@gmail.com>
2025-11-16 12:38:45 +0700

    1

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.project1
 
 
+import FilmListRecyclerAdapter
 import android.animation.ObjectAnimator
+import android.content.res.Resources
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.ActionMode
 import android.view.View
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.example.project1.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
-
+    private lateinit var filmsAdapter: FilmListRecyclerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -59,4 +63,24 @@ class MainActivity : ComponentActivity() {
         }
 
     }
+
+    val filmsDataBase = listOf(
+        Film("Film title", R.drawable.q39q, "This should be a description"),
+        Film("Film title", R.drawable.q39q, "This should be a description"),
+
+    )
 }
+class TopSpacingItemDecoration (private val paddingInDp: Int): RecyclerView.ItemDecoration() {
+    private val Int.convertPx: Int
+        get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        super.getItemOffsets(outRect, view, parent, state)
+        outRect.top = paddingInDp.convertPx
+        outRect.right = paddingInDp.convertPx
+        outRect.left = paddingInDp.convertPx
+
+    }
+}
+data class Film(val title: String, val poster: Int, val description: String)
+
