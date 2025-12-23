52912cb9e3b033330ea15e4c5954a0b48145d643
yuri <mortus259@gmail.com>
2025-12-23 21:20:07 +0700

    1

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.project1
 
-// ДОБАВЬТЕ ЭТУ СТРОКУ, ЕСЛИ DetailsActivity находится в этом же пакете,
-// но не был импортирован.
 import Film
 import FilmListRecyclerAdapter
 import TopSpacingItemDecoration
@@ -9,21 +7,46 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.project1.databinding.ActivityMainBinding
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
     private val filmsDataBase = listOf(
         Film("Воображаемый друг", R.drawable.ffmqt, "This should be a description"),
         Film("Пила Наследие", R.drawable.lgncz, "This should be a description"),
-        Film("The Godfather", R.drawable.the_godfather, "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."),
-        Film("The Godfather", R.drawable.the_godfather, "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."),
-        Film("The Godfather", R.drawable.the_godfather, "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."),
-        Film("The Godfather", R.drawable.the_godfather, "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to its reluctant son."),
-        Film("The Godfather", R.drawable.the_godfather, "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to its reluctant son."),
-        Film("The Godfather", R.drawable.the_godfather, "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to its reluctant son.")
+        Film(
+            "The Godfather",
+            R.drawable.the_godfather,
+            "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."
+        ),
+        Film(
+            "The Godfather",
+            R.drawable.the_godfather,
+            "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."
+        ),
+        Film(
+            "The Godfather",
+            R.drawable.the_godfather,
+            "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."
+        ),
+        Film(
+            "The Godfather",
+            R.drawable.the_godfather,
+            "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to its reluctant son."
+        ),
+        Film(
+            "The Godfather",
+            R.drawable.the_godfather,
+            "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to its reluctant son."
+        ),
+        Film(
+            "The Godfather",
+            R.drawable.the_godfather,
+            "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to its reluctant son."
+        )
     )
 
     private lateinit var filmsAdapter: FilmListRecyclerAdapter
@@ -31,7 +54,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initNavigation()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_placeholder, HomeFragment())
+            .addToBackStack(null)
+            .commit()
     }
+
 
     private fun initNavigation() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -44,10 +72,12 @@ class MainActivity : ComponentActivity() {
                     Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
                     true
                 }
+
                 R.id.topAppBar -> {
                     Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
                     true
                 }
+
                 else -> false
             }
         }
@@ -59,37 +89,41 @@ class MainActivity : ComponentActivity() {
                     Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
                     true
                 }
+
                 R.id.watch_later -> {
                     Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
                     true
                 }
+
                 R.id.selections -> {
                     Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
                     true
                 }
+
                 else -> false
             }
         }
 
         // --- Инициализация RecyclerView ---
         binding.mainRecycler.apply {
-            filmsAdapter = FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener{
-                override fun click(film: Film) {
-                    //Создаем бандл и кладем туда объект с данными фильма
-                    val bundle = Bundle()
-                    bundle.putParcelable("film", film)
+            filmsAdapter =
+                FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener {
+                    override fun click(film: Film) {
+                        //Создаем бандл и кладем туда объект с данными фильма
+                        val bundle = Bundle()
+                        bundle.putParcelable("film", film)
 
-                    // Запускаем наше активити
-                    val intent = Intent(this@MainActivity, DetailsActivity::class.java)
+                        // Запускаем наше активити
+                        val intent = Intent(this@MainActivity, DetailsActivity::class.java)
 
-                    // Прикрепляем бандл к интенту
-                    // putExtras - это правильный метод для Intent
-                    intent.putExtras(bundle)
+                        // Прикрепляем бандл к интенту
+                        // putExtras - это правильный метод для Intent
+                        intent.putExtras(bundle)
 
-                    // Запускаем активити через интент
-                    startActivity(intent)
-                }
-            })
+                        // Запускаем активити через интент
+                        startActivity(intent)
+                    }
+                })
 
             adapter = filmsAdapter
             layoutManager = LinearLayoutManager(this@MainActivity)
