0be6b49d6b38482c990b02420b52a879869a909f
Yuri <mortus259@gmail.com>
2026-05-22 06:50:14 +0700

    Теперь при клике на поле ввода появляется клавиатура

--- app/src/main/java/com/example/project1/HomeFragment.kt
+++ app/src/main/java/com/example/project1/HomeFragment.kt
@@ -6,9 +6,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.project1.databinding.FragmentHomeBinding
+import java.util.Locale
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -28,11 +30,12 @@ class HomeFragment : Fragment() {
 
         // Находим RecyclerView через binding
         binding.mainRecycler.apply {
-            filmsAdapter = FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener {
-                override fun click(film: Film) {
-                    (activity as MainActivity).launchDetailsFragment(film)
-                }
-            })
+            filmsAdapter =
+                FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener {
+                    override fun click(film: Film) {
+                        (activity as MainActivity).launchDetailsFragment(film)
+                    }
+                })
             adapter = filmsAdapter
             layoutManager = LinearLayoutManager(requireContext())
             // Добавляем декоратор (отступы)
@@ -42,10 +45,59 @@ class HomeFragment : Fragment() {
         // Получаем данные из MainActivity и передаем в адаптер
         val data = (activity as MainActivity).filmsDataBase
         filmsAdapter.addItems(data)
+        val search_view = binding.searchView
+        search_view.isIconfiedByDefault
+        search_view.clearFocus()
+        search_view.setOnClickListener {
+            search_view.isIconified = false // Разворачиваем
+            search_view.requestFocus()      //
+        }
+
+
+        //Подключаем слушателя изменений введенного текста в поиска
+        search_view.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            //Этот метод отрабатывает при нажатии кнопки "поиск" на софт клавиатуре
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                TODO("Not yet implemented")
+            }
+            //Этот метод отрабатывает на каждое изменения текста
+            override fun onQueryTextChange(newText: String?): Boolean {
+                TODO("Not yet implemented")
+            }
+        })
+
+        search_view.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            //Этот метод отрабатывает при нажатии кнопки "поиск" на софт клавиатуре
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+            //Этот метод отрабатывает на каждое изменения текста
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                //Если ввод пуст то вставляем в адаптер всю БД
+                val result=(activity as MainActivity).filmsDataBase.filter
+                if (newText.isEmpty()) {
+                    filmsAdapter.addItems( filmsDataBase)
+                    return true
+                }
+                //Фильтруем список на поискк подходящих сочетаний
+
+                 val result = filmsDataBase.filter {
+                    //Чтобы все работало правильно, нужно и запрос, и имя фильма приводить к нижнему регистру
+                    it.title.lowercase(Locale.getDefault()).contains(newText.lowercase(Locale.getDefault()))
+                }
+                //Добавляем в адаптер
+                filmsAdapter.addItems(result)
+                return true
+            }
+        })
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
 }
